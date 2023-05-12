527aeaa7e0c537c9c259358f2f7f15a490680fcb
cws <kyz9880@naver.com>
2023-05-12 14:59:17 +0900

    tw

--- src/main/java/com/btone/dev/developerborad/HelloController.java
+++ src/main/java/com/btone/dev/developerborad/HelloController.java
@@ -1,9 +1,12 @@
 package com.btone.dev.developerborad;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 @RestController
+@RequestMapping("/api")
 public class HelloController {
-    @GetMapping("/api/hello")
+    @GetMapping("/hello")
     public String test() {
         return "Hello, w12312323orld!";
     }
